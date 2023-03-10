c31ed0863ccc79e463326d2ace6df352b42da431
Don <496929151@qq.com>
2023-03-10 15:41:51 +0800

    chore: init

---
+++ App.vue
@@ -0,0 +1,8 @@
+<script setup>
+	import {
+		onReady
+	} from '@dcloudio/uni-app'
+	onReady(() => {
+		console.log('onReady')
+	})
+</script>
---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<view class="abc">
+			{{counter}}
+		</view>
+	</view>
+</template>
+<script setup>
+	import {
+		useCounterStore
+	} from '@/stores/counter';
+	const counter = useCounterStore();
+
+	counter.count++;
+	// 可以手动触发
+	counter.$patch({
+		count: counter.count + 1
+	});
+	// 或者使用 actions
+	counter.increment();
+</script>
+
+<style lang="scss">
+	.abc{
+		background-color: red;
+	}
+</style>
